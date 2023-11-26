e561bc862e26dd8e565b588016f070b694d97029
Joaquim Mulaza <joaquimmulazadev@gmail.com>
2023-11-26 23:22:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,171 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+
+const JWTSecret = "fndkfnkdsnkfndsfndskfkldsfkldsfknfksfnsknndskfn"
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+var DB = {
+    games: [
+        {
+            id: 23,
+            title: "Call of duty MW",
+            year: 2019,
+            price: 60
+        },
+        {
+            id: 65,
+            title: "Sea of thieves",
+            year: 2018,
+            price: 40
+        },
+        {
+            id: 2,
+            title: "Minecraft",
+            year: 2012,
+            price: 20
+        }
+    ],
+
+    users:[
+        {
+            id: 1,
+            name:"Joaquim Mulaza",
+            email: "joaquimmulazadev@gmail.com",
+            password: "0000"
+        },
+
+        {
+            id: 4,
+            name:"Delcio Figueiro",
+            email: "delciofigueiredo@gmail.com",
+            password: "1234"
+        }
+    ]
+}
+
+app.get("/games",(req, res) => {
+    res.statusCode = 200;
+    res.json(DB.games);
+});
+
+app.get("/game/:id",(req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        
+        var id = parseInt(req.params.id);
+
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined){
+            res.statusCode = 200;
+            res.json(game);
+        }else{
+            res.sendStatus(404);
+        }
+    }
+});
+
+app.post("/game",(req, res) => { 
+    var {title, price, year} = req.body;
+    DB.games.push({
+        id: 2323,
+        title,
+        price,
+        year
+    });
+    res.sendStatus(200);
+})
+
+app.delete("/game/:id",(req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        var id = parseInt(req.params.id);
+        var index = DB.games.findIndex(g => g.id == id);
+
+        if(index == -1){
+            res.sendStatus(404);
+        }else{
+            DB.games.splice(index,1);
+            res.sendStatus(200);
+        }
+    }
+});
+
+app.put("/game/:id",(req, res) => {
+
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        
+        var id = parseInt(req.params.id);
+
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined){
+
+            var {title, price, year} = req.body;
+
+            
+            if(title != undefined){
+                game.title = title;
+            }
+
+            if(price != undefined){
+                game.price = price;
+            }
+
+            if(year != undefined){
+                game.year = year;
+            }
+            
+            res.sendStatus(200);
+
+        }else{
+            res.sendStatus(404);
+        }
+    }
+
+});
+
+app.post("/auth", (req, res) => {
+    var {email, password} = req.body
+
+    if(email != undefined) {
+        var user = DB.users.find(u => u.email == email);
+
+        if(user != undefined) {
+            if(user.password == password) {
+
+                jwt.sign({id: user.id, email: user.email}, JWTSecret,{expiresIn:'48h'}, (err, token) => {
+                    if(err) {
+                        res.status(400);
+                        res.json({err: "Falha interna"})
+                    } else {
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                })
+            } else {
+                res.status(401);
+                res.json({err: "Credenciais invalidas"})
+            }
+        } else {
+            res.status(404);
+            res.json({err: "E-mail enviado nao existe na base de dados"})
+        }
+    } else {
+        res.status(400);
+        res.json({err: "E-mail enviado invalido"})
+    }
+})
+
+app.listen(45678,() => {
+    console.log("API RODANDO!");
+});
